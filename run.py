8056fdc1302faa597dcaeae09f52016d785fd7c1
Asadi-Mehdi <asadi128@gmail.com>
2026-07-26 09:34:56 +0330

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,16 @@
+from fedaegis import __version__
 
+
+def main():
+
+    print("=" * 45)
+    print(f"FedAegis-X {__version__}")
+    print("=" * 45)
+
+    print("Framework Status : READY")
+    print("Execution Stage  : Bootstrap")
+    print("Next Stage       : Dataset Loader")
+
+
+if __name__ == "__main__":
+    main()
