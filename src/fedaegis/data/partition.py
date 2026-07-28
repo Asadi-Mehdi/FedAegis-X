20b52984b7bd889055b98cd154abd50bf36c393d
Asadi-Mehdi <asadi128@gmail.com>
2026-07-28 10:31:59 +0330

    Update partition.py

--- src/fedaegis/data/partition.py
+++ src/fedaegis/data/partition.py
@@ -1,10 +1,32 @@
 import numpy as np
 
 
-def iid_partition(X, y, clients):
+def iid_partition(
+        X,
+        y,
+        clients):
 
-    x_parts = np.array_split(X, clients)
+    idx = np.random.permutation(
+        len(X)
+    )
 
-    y_parts = np.array_split(y, clients)
+    X = X[idx]
 
-    return list(zip(x_parts, y_parts))
+    y = y[idx]
+
+    x_parts = np.array_split(
+        X,
+        clients
+    )
+
+    y_parts = np.array_split(
+        y,
+        clients
+    )
+
+    return list(
+        zip(
+            x_parts,
+            y_parts
+        )
+    )
