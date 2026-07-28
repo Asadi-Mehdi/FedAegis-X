976d096ff5a93f026fb6fc74b6c04cab601a22ed
Asadi-Mehdi <asadi128@gmail.com>
2026-07-28 11:47:29 +0330

    Update client.py

--- src/fedaegis/client.py
+++ src/fedaegis/client.py
@@ -8,11 +8,8 @@ class Client:
             y):
 
         self.client_id = client_id
-
         self.model = model
-
         self.X = X
-
         self.y = y
 
     def train(self):
@@ -24,6 +21,16 @@ class Client:
 
         return self.model.get_parameters()
 
-    def predict(self, X):
+    def update_global_model(
+            self,
+            global_parameters):
+
+        self.model.set_parameters(
+            global_parameters
+        )
+
+    def predict(
+            self,
+            X):
 
         return self.model.predict(X)
