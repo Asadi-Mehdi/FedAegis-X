ee95207dd07b028d493f2a8f40b4dfb0ca29aee6
Asadi-Mehdi <asadi128@gmail.com>
2026-08-15 09:23:05 +0330

    Create client_report.py

---
+++ src/fedaegis/core/client_report.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+
+@dataclass
+
+class ClientReport:
+
+    client_id: int
+
+    samples: int
+
+    fnr: float
+
+    class_balance: float
+
+    reliability: float
+
+    parameters: dict
